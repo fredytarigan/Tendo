8e1459d088e78d1013e359108262df51fa3a0344
Fredy Samuel B Tarigan <fredy.tarigan@tiptip.tv>
2024-08-06 14:52:35 +0700

    start working on readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Tendo
+
+A simple and basic tools to auto update Tencent SSL Certificate based on Cert-Manager certificate secret.
+
--- dockerbuild/entrypoint.sh
+++ dockerbuild/entrypoint.sh
@@ -1,15 +1,2 @@
-# check for vault secret files before running
-APP_CONFIG=/vault/secrets/config
-DATABASE_CONFIG=/vault/secrets/database
-if [ -f "$APP_CONFIG" ];
-then
-    source "$APP_CONFIG"
-fi
-
-if [ -f "$DATABASE_CONFIG" ];
-then
-    source "$DATABASE_CONFIG"
-fi
-
 # running actual command
 exec ./app "$@"
